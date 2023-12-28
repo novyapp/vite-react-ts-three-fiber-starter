52265431a0db2e257d0f98c82264168568cb34b4
NovyAPP <micnowotnik@gmail.com>
2023-12-28 14:05:24 +0100

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+
 function App() {
-  return;
+  return (
+    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+      <color attach="background" args={["#ececec"]} />
+      <Experience />
+    </Canvas>
+  );
 }
 
 export default App;
